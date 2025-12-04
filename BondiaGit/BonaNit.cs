fcff06c1b5d3cc09323f9d0f9179809aaac4ead4
WassimaTajioui <440034.clot@fje.edu>
2025-12-04 13:43:44 +0100

    Agregar archivos de proyecto.

---
+++ BondiaGit/BonaNit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BondiaGit
+{
+    public class BonaNit
+    {
+        public void Saludar()
+        {
+            Console.WriteLine("Bona nit");
+        }
+    }
+}
---
+++ BondiaGit/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BondiaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BonDia bonDia = new BonDia();
+            BonaNit bonaNit = new BonaNit();
+
+            bonDia.Saludar();
+            bonaNit.Saludar();
+        }
+    }
+}
