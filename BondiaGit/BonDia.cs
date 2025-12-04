7ba08d9227d96847704889f4daa806f27eb5e33e
SamiraAbouhssen <445854.clot@fje.edu>
2025-12-04 20:19:22 +0100

    Millorar salutació de la nit.

--- BondiaGit/BonDia.cs
+++ BondiaGit/BonDia.cs
@@ -10,7 +10,7 @@ namespace BondiaGit
     {
         public void Saludar()
         {
-            Console.WriteLine("Bon dia! Espero que tinguis un gran dia!");
+            Console.WriteLine("Bona nit! Dorm bé i somia fort!");
         }
     }
 }
--- BondiaGit/BonaNit.cs
+++ BondiaGit/BonaNit.cs
@@ -10,7 +10,7 @@ namespace BondiaGit
     {
         public void Saludar()
         {
-            Console.WriteLine("Bona nit");
+            Console.WriteLine("Bona nit!");
         }
     }
 }
